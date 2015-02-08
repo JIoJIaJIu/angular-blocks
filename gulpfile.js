9d67661c159d1eb1424f4492058a1807768371d5
Guro Bokum <jiojiajiu@gmail.com>
2015-02-08 18:43:01 +0700

    Watcher

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,12 @@ gulp.task('build', function () {
         .pipe(concat('angular-blocks.js'))
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch('libs/*.js')
+        .on('change', function () {
+            gulp.src('libs/*.js')
+                .pipe(concat('angular-blocks.js'))
+                .pipe(gulp.dest('./'));
+        });
+});
