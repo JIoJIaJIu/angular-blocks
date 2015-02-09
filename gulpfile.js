c321261e1e67b21e456b08f057254fc9b4b0825b
Guro Bokum <jiojiajiu@gmail.com>
2015-02-09 13:29:02 +0700

    guplfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+gulp.task('default', function () {
+    gulp.run('build');
+});
 
 gulp.task('build', function () {
     gulp.src('libs/*.js')
         .pipe(concat('angular-blocks.js'))
+        .pipe(gulp.dest('./'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function () {
     gulp.watch('libs/*.js')
         .on('change', function () {
-            gulp.src('libs/*.js')
-                .pipe(concat('angular-blocks.js'))
-                .pipe(gulp.dest('./'));
+            gulp.run('build');
         });
 });
